221e0dd751530874435b9add8cc023cfacd21178
alex123321-maker <alexander12110@gmail.com>
2023-08-16 21:41:27 +1200

    Clients

---
+++ Models/CustomerModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace hotel.Models
+{
+    public class CustomerModels
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Passportseries { get; set; }
+        public int Passportnumber { get; set; }
+        public string Prefers{ get; set; }
+
+    }
+}
---
+++ Models/ModelsContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace hotel.Models
+{
+    public class ModelsContext:DbContext
+    {
+        public DbSet<CustomerModels> Customers { get; set; }
+
+    }
+}
